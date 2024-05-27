7c4a243a4285a7c1cf887f449e7d8769dbe5daca
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-27 20:48:39 +0300

    Add files via upload

---
+++ While Loop/Old Books/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Input
+            string Mybook = Console.ReadLine();
+            string nextbook = Console.ReadLine();
+            int count = 0;
+          //Logic
+            while (nextbook != "No More Books")
+            {
+                if (Mybook==nextbook)
+                {
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    return;
+                }
+                nextbook = Console.ReadLine();
+                count++;
+            }
+            // result
+            if (nextbook== "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");
+            }
+           
+        }
+    }
+}
