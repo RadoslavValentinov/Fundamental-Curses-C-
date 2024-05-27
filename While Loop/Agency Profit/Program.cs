60e6a21feeef04a249fbbdb8f7998dd052749534
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-05-27 20:46:21 +0300

    Add files via upload

---
+++ While Loop/Agency Profit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AgencyProfit
+{
+    class Program
+    {
+        static void Main()
+        {
+            string aviocumpani = Console.ReadLine();
+            double ticketsgrownup = double.Parse(Console.ReadLine());
+            double ticketschildren = double.Parse(Console.ReadLine());
+            double Netprice = double.Parse(Console.ReadLine());
+            double servicecharge = double.Parse(Console.ReadLine());
+            double childrentiket =Netprice- Netprice * 0.7 + (servicecharge);
+            double ticketgrow = Netprice + servicecharge;
+            double total = childrentiket * ticketschildren + ticketgrow * ticketsgrownup;
+            Console.WriteLine($"The profit of your agency from {aviocumpani} tickets is {total * 0.2:f2} lv.");
+        }
+    }
+}
